bff3e13224ee7a5f03c48226e0c62adb8efd96cf
Preet <2000vermapreet@gmail.com>
2026-08-16 00:16:28 +0530

    cors error resolved

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 
 const userRoutes = require("./routes/userRoutes");
@@ -10,6 +11,7 @@ const swaggerSpec = require("./config/swagger.config");
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
